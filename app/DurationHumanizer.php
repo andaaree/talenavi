b6ef03eb3c05b511ccbbf71aa04e3be62ecbff89
Andar <50786504+andaaree@users.noreply.github.com>
2025-10-23 20:08:59 +0700

    feat(excel): add diff function
    - [] add diff function to get timestamps difference in seconds

--- app/DurationHumanizer.php
+++ app/DurationHumanizer.php
@@ -11,5 +11,9 @@ trait DurationHumanizer
         return CarbonInterval::seconds($totalSeconds)->cascade()->format('%H:%I:%S');
     }
 
+    public function diff($a,$b){
+        $timeA = Carbon::parse($a);
+        $timeB = Carbon::parse($b);
+        return $timeA->diffInSeconds($timeB);
     }
 }
