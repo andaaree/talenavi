7365c4dcb2e125320c8927dc472b6fa15f697f2f
Andar <50786504+andaaree@users.noreply.github.com>
2025-10-24 17:42:24 +0700

    refactor(todo): change guarded to fillable in model
    - To make it easier to call in controller

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -13,7 +13,14 @@ class Todo extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'title',
+        'assignee',
+        'time_tracked',
+        'status',
+        'priority',
+        'due_date',
+    ];
 
     protected $casts = [
       'due_date'=> 'datetime:d-m-Y',
