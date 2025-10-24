a0948bcb0cc265902712da06af6dca33e47e956b
Andar <50786504+andaaree@users.noreply.github.com>
2025-10-24 13:12:37 +0700

    Revert "feat(todo): add todo seeder"
    This reverts commit 2b7d896e6ea3801f181c525a77ccee3da01b40f5.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call(TodoSeeder::class);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
